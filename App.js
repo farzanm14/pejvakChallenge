db5441bea136387feb52c94fff7e009652075509
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-11-29 16:29:44 +0330

    add nativeBase as uiComponent

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/routes/AuthStack';
+import { StyleProvider, Root } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material'; 
 
 const App = () => {
+  console.disableYellowBox = true;
+
   return (
-    <>
-      <NavigationContainer>
-        <AuthStack/>
-      </NavigationContainer>
-    </>
+      <StyleProvider style={getTheme(material)}>
+        <Root>
+          <NavigationContainer>
+            <AuthStack />
+          </NavigationContainer>
+        </Root>
+      </StyleProvider>
   );
 };
 
