26b39852a872aa6439c411c35c99de03a8db98fd
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-11-30 13:21:32 +0330

    create custom input component

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import { Button, Text, View, Icon, Input, Item, Container } from 'native-base';
+import OTPInputView from '@twotalltotems/react-native-otp-input'
 
 const CustomInput = ({ length, callBack, hintText }) => {
-    
+    const [value, setValue] = React.useState('')
+
     return (
         <View style={styles.container}>
             <Text style={styles.hintStyle}>{hintText}</Text>
-            <Input 
-                maxLength={length}
-                style={{}}
-                keyboardType='phone-pad'
-                value={'phone'}
-                // onChangeText={(phone) => setPhone(phone)}
+            <OTPInputView
+                style={styles.theInput}
+                pinCount={length}
+                code={value} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
+                onCodeChanged={code => setValue(code)}
+                autoFocusOnLoad
+                editable
+                selectionColor='black'
+                keyboardAppearance='dark'
+                keyboardType='number-pad'
+                placeholderTextColor='gray'
+                codeInputFieldStyle={styles.underlineStyleBase}
+                codeInputHighlightStyle={styles.underlineStyleHighLighted}
+                onCodeFilled={(code) => callBack(code)}
             />
         </View>
     )
 }
 
-export default CustomInput
+export default CustomInput;
 
 
 const styles = StyleSheet.create({
@@ -27,10 +37,33 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 5,
         flexDirection: 'row',
-        paddingHorizontal:5
+        paddingHorizontal: 5,
+        height: 45
     },
     hintStyle: {
         color: 'gray',
         alignSelf: 'center'
+    },
+    underlineStyleBase: {
+        width: 14,
+        height: 20,
+        color:'black',
+        borderColor: "gray",
+        fontFamily:'IRANSansWeb(FaNum)',
+        // backgroundColor:'green',
+        borderWidth: 0,
+        borderBottomWidth: 1,
+    },
+
+    underlineStyleHighLighted: {
+        borderColor: "gray",
+    },
+    theInput: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        color: 'gray',
+        fontSize: 20,
+        marginHorizontal: 10,
+        height: 10,
     }
-})
+});
--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -4,26 +4,27 @@ import { Image, SafeAreaView, KeyboardAvoidingView } from 'react-native';
 import styles from '../../styles/auth/loginStyles'
 import PatternBackground from '../../components/PatternBackground'
 import CustomInput from '../../components/CustomInput'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 function LoginScreen({ navigation }) {
   const [phone, setPhone] = React.useState('')
 
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+    <KeyboardAwareScrollView>
       <PatternBackground>
         <SafeAreaView style={styles.container}>
           <View style={styles.formContainer}>
             <Text style={styles.introText}>برای ورود شماره تماس خود را وارد نمایید</Text>
-            <CustomInput length={11} callBack={val=>alert(val)} hintText='+۹۸' style={styles.input}/>
-            <Button block full iconLeft  onPress={() => { navigation.navigate('DrawerNavigation'); }} style={styles.button}>
+            <CustomInput length={10} callBack={val => alert(val)} hintText='+۹۸' style={styles.input} />
+            <Button block full iconLeft onPress={() => { navigation.navigate('DrawerNavigation'); }} style={styles.button}>
               <Icon type="MaterialIcons" name="keyboard-arrow-left" />
               <Text>ورود</Text>
             </Button>
           </View>
         </SafeAreaView>
       </PatternBackground>
-    </KeyboardAvoidingView>
+    </KeyboardAwareScrollView>
   );
 }
 export default LoginScreen;
